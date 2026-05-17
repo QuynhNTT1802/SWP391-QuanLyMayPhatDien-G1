b195b41fc8987ef5b7c0d7f564712154b39e9563
Phuong Linh <linhlinhlinh58206@gmail.com>
2026-05-17 10:06:05 +0700

    Not change (Push for Q)

--- src/main/java/com/quanlymayphatdien/g1/dal/DBContext.java
+++ src/main/java/com/quanlymayphatdien/g1/dal/DBContext.java
@@ -21,7 +21,7 @@ public class DBContext {
     public DBContext() {
         try {
             String username = "root";
-            String password = "1234";
+            String password = "123456789";
             String url = "jdbc:mysql://localhost:3306/warehousedb";
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(url, username, password);
