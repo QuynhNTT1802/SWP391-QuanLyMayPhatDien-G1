d0d8016e0b5420b17d9c6d11048c7e8958549a70
anhsnkaa <anhsnkaa@gmail.com>
2026-05-19 00:56:23 +0700

    add auto gen password

--- src/main/java/com/quanlymayphatdien/g1/config/GlobalConfig.java
+++ src/main/java/com/quanlymayphatdien/g1/config/GlobalConfig.java
@@ -1,13 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.quanlymayphatdien.g1.config;
 
-/**
- *
- * @author Aadmin
- */
 public class GlobalConfig {
     
+    public static final String REGEX_USERNAME = "^[a-zA-Z0-9_]+$";
+    
+    public static final String REGEX_PASSWORD = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$";
+    
+    public static final String REGEX_EMAIL = 
+        "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+    
+    public static final String REGEX_PHONE = "^0[0-9]{9,10}$";
 }
