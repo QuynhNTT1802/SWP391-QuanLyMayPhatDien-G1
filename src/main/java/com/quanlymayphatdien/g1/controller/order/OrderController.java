ee5747b5613e194b1a637e72e4cad640396e6aa2
linhlinhlinh582006-glitch <linhlinhlinh58206@gmail.com>
2026-05-27 14:06:31 +0700

    Xong 90% con generator push tam cho tanh

--- src/main/java/com/quanlymayphatdien/g1/controller/order/OrderController.java
+++ src/main/java/com/quanlymayphatdien/g1/controller/order/OrderController.java
@@ -176,6 +176,7 @@ public class OrderController extends HttpServlet {
 
         request.getRequestDispatcher("/view/order/list.jsp").forward(request, response);
     }
+     
 
     private void viewDetail(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
