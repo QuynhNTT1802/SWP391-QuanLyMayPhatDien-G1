9b54af722a7273b8e1dfd7db313c6a86f46cd809
Khanhnv26 <vankhanhak54@gmail.com>
2026-06-26 15:34:39 +0700

    Iteration 2

--- src/main/webapp/assets/js/searchable-dropdown.js
+++ src/main/webapp/assets/js/searchable-dropdown.js
@@ -165,7 +165,6 @@
                   +     '</div>'
                   +     '<div class="cust-card-right">'
                   +         '<span class="cust-card-badge ' + badgeClass + '">' + badgeText + '</span>'
-                  +         '<div class="cust-card-icon"><svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M9 18l6-6-6-6"/></svg></div>'
                   +     '</div>'
                   + '</div>';
         }
