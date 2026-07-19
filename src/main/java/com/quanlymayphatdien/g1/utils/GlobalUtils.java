bb43fb069494931322fff15aaa334af37163ea6c
QuynhNTT1802 <nt.thuyquynh1802@gmail.com>
2026-07-20 01:40:54 +0700

    merge voi tienanh

--- src/main/java/com/quanlymayphatdien/g1/utils/GlobalUtils.java
+++ src/main/java/com/quanlymayphatdien/g1/utils/GlobalUtils.java
@@ -52,8 +52,4 @@ public class GlobalUtils {
     public static final String TRANSFER_STATUS_COMPLETED = "COMPLETED";
     public static final String TRANSFER_STATUS_REJECTED = "REJECTED";
     public static final String TRANSFER_STATUS_REQUEST_REVISION = "REQUEST_REVISION";
-<<<<<<< HEAD
-
-=======
->>>>>>> cf857ca9744428e34c1eb3a571113d45a128a435
 }
